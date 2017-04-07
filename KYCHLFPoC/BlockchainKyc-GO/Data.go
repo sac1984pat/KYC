976d244d02b79e8b45245a5e803bfa6d444e0a85
sac1984pat <sac1984pat@gmail.com>
2017-04-07 16:43:00 +0530

    Multiple go files with table insertion

---
+++ KYCHLFPoC/BlockchainKyc-GO/Data.go
@@ -0,0 +1,16 @@
+package main
+
+type KycData struct {
+	USER_NAME           string `json:"USER_NAME"`
+	USER_ID             string `json:"USER_ID"`
+	KYC_BANK_NAME       string `json:"KYC_BANK_NAME"`
+	KYC_CREATE_DATE     string `json:"KYC_CREATE_DATE"`
+	KYC_VALID_TILL_DATE string `json:"KYC_VALID_TILL_DATE"`
+	KYC_DOC_BLOB        string `json:"KYC_DOC_BLOB"`
+}
+
+type KycCount struct {
+	AllContracts      int
+	ExpiringContracts int
+	CreatedContracts  int
+}
